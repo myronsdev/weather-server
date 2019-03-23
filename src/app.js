ce6d92fca5d45ee83447357810c8506698633cce
Myron Mctyer <myronsdev@gmail.com>
2019-03-22 20:20:11 -0500

    Create static pages and create public dir

--- src/app.js
+++ src/app.js
@@ -1,18 +1,15 @@
+const path = require('path')
 const express = require('express')
 
 const app = express()
 
-app.get('', (req, res) => {
-  res.send('<h1>Weather</h1>')
-})
+const publicDirectoryPath = path.join(__dirname, '../public')
 
-app.get('/help', (req, res) => {
-  res.send([{ name: 'Myron' }, { age: 32 }])
-})
+app.use(express.static(publicDirectoryPath))
 
-app.get('/about', (req, res) => {
-  res.send('<h2>About Page</h2>')
-})
+// app.get('', (req, res) => {
+//   res.send('<h1>Weather</h1>')
+// })
 
 app.get('/weather', (req, res) => {
   res.send({
