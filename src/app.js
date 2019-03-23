8fd050658c955f5b19a7490f815f21d4e7f4cfc8
Myron Mctyer <myronsdev@gmail.com>
2019-03-22 23:41:35 -0500

    Add hbs templates and setup get functions to handle them

--- src/app.js
+++ src/app.js
@@ -5,7 +5,28 @@ const app = express()
 
 const publicDirectoryPath = path.join(__dirname, '../public')
 
+app.set('view engine', 'hbs')
 app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+  res.render('index', {
+    title: 'Weather',
+    name: 'Myron'
+  })
+})
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'About Me',
+    name: 'Myron'
+  })
+})
+
+app.get('/help', (req, res) => {
+  res.render('help', {
+    helpMsg: 'This app will provide weather for any location'
+  })
+})
 
 app.get('/weather', (req, res) => {
   res.send({
