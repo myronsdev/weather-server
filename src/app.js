f9c5c72ca8c4c99a4f31402e16a0b31ef39e62ee
Myron Mctyer <myronsdev@gmail.com>
2019-03-24 00:01:08 -0500

    Refactor weather route

--- src/app.js
+++ src/app.js
@@ -45,35 +45,30 @@ app.get('/help', (req, res) => {
 app.get('/weather', (req, res) => {
   if (!req.query.address) {
     return res.send({
-      error: 'Must provide an address'
+      error: 'You must provide an address!'
     })
   }
 
-  geocode(req.query.address, (error, { latitude, longitude, location }) => {
-    if (location) {
+  geocode(
+    req.query.address,
+    (error, { latitude, longitude, location } = {}) => {
       if (error) {
-        return res.send({
-          error
-        })
+        return res.send({ error })
       }
-      forecast(latitude, longitude, (error, forcastData) => {
+
+      forecast(latitude, longitude, (error, forecastData) => {
         if (error) {
-          return res.send({
-            error: error
-          })
+          return res.send({ error })
         }
+
         res.send({
-          forecast: forcastData,
+          forecast: forecastData,
           location,
           address: req.query.address
         })
       })
-    } else {
-      res.send({
-        error: 'Please provide a location'
-      })
     }
-  })
+  )
 })
 
 app.get('/products', (req, res) => {
