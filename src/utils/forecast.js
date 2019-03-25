9c84075453dd382f58c190ede6900fe0fece29c8
Myron Mctyer <myronsdev@gmail.com>
2019-03-25 13:22:42 -0500

    Update weather forecast to include windSpeed

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -16,7 +16,10 @@ const forecast = (latitude, longitude, callback) => {
           body.currently.temperature +
           ' degress out. There is a ' +
           body.currently.precipProbability +
-          '% chance of rain.'
+          '% chance of rain' +
+          ' and the wind is blowing at ' +
+          body.currently.windSpeed +
+          ' mph.'
       )
     }
   })
